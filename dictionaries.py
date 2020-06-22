dcfe8a3a232b9183e3aac70aec964d325a22b0c2
YingerYu <yuyu_is@jaist.ac.jp>
2020-06-22 17:05:24 +0900

    dictionary

---
+++ dictionaries.py
@@ -0,0 +1,20 @@
+# Worked Exercises.009
+
+name = input('Enter file:')
+if len(name) < 1:
+    name = 'Readme.md'
+handle = open(name)
+
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0)+1
+bigcount = None
+bigword = None
+for word, count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+
+print(bigword, bigcount)
