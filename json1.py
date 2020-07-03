53cccb25d9102eb2f96ac3fd42834af2f1c0ba5c
Yuiu-PhD <kanmiyounyu@gmail.com>
2020-07-03 16:26:35 +0900

    xml and json examples

---
+++ json1.py
@@ -0,0 +1,25 @@
+import json
+
+data = '''
+[
+  { "id" : "001",
+    "x" : "2",
+    "name" : "Chuck"
+  } ,
+  { "id" : "009",
+    "x" : "7",
+    "name" : "Brent"
+  } ,
+  { "id" : "007",
+    "x" : "11.3",
+    "name" : "Hulkis a cat"
+  }
+]'''
+
+info = json.loads(data)
+print('User count:', len(info))
+
+for item in info:
+    print('Name', item['name'])
+    print('Id', item['id'])
+    print('Attribute', item['x'])
---
+++ xml1.py
@@ -0,0 +1,28 @@
+import xml.etree.ElementTree as ET
+
+input = '''
+<stuff>
+  <users>
+    <user x="2">
+      <id>001</id>
+      <name>Chuck</name>
+    </user>
+    <user x="7">
+      <id>009</id>
+      <name>Brent</name>    
+    </user>
+    <user x="11.3">
+      <id>007</id>
+      <name>Hulk is a cat</name>    
+    </user>
+  </users>
+</stuff>'''
+
+stuff = ET.fromstring(input)
+lst = stuff.findall('users/user')
+print('User count:', len(lst))
+
+for item in lst:
+    print('Name', item.find('name').text)
+    print('Id', item.find('id').text)
+    print('Attribute', item.get('x'))
